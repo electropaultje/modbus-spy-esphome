0445614dc3e70813eac5a9237938adb5817f8569
Ferdinand Oeinck <fo@bigroses.net>
2026-03-10 16:54:18 +0100

    supprt more registers for fc16

--- components/modbus_spy/modbus_data_splitter.cpp
+++ components/modbus_spy/modbus_data_splitter.cpp
@@ -138,39 +138,50 @@ vector<ModbusData*>* ModbusDataSplitter::handle_function_16(ModbusFrame* request
   // No repsonse with data for FC 16 
   uint8_t *request_data = request->get_data();
   // Passed all tests! Let's get the register's address, and the value set in it
-  uint8_t address_high_byte = request_data[0];
-  uint8_t address_low_byte = request_data[1];
-  uint16_t address = (address_high_byte << 8) | address_low_byte;
+  uint8_t start_address_high_byte = request_data[0];
+  uint8_t start_address_low_byte = request_data[1];
+  uint16_t start_address = (start_address_high_byte << 8) | start_address_low_byte;
 
-  uint8_t length_high_byte = request_data[2];
-  uint8_t length_low_byte = request_data[3];
-  uint16_t length = (length_high_byte << 8) | length_low_byte;
-  
-  if (length != 1)
-  {
-    ESP_LOGD(TAG, "Number of registers for function 16 is not 1, but %d", length);
+  uint8_t register_count_requested_high_byte = request_data[2];
+  uint8_t register_count_requested_low_byte = request_data[3];
+  uint16_t register_count_requested = (register_count_requested_high_byte << 8) | register_count_requested_low_byte;
+
+  uint16_t expected_bytes_in_response = register_count_requested * 2 + 2;
+  if (response->get_data_length() != expected_bytes_in_response) {
+    ESP_LOGD(TAG, "Response data length for function 16 does not match expected length: expected %d, got %d",
+             expected_bytes_in_response, response->get_data_length());
     return nullptr;
   }
 
-  uint8_t bytes = request_data[4];
-  if (bytes != 2)
+  uint8_t data_bytes = request_data[4];
+  if (data_bytes != 2 * register_count_requested)
   {
-    ESP_LOGD(TAG, "Number of bytes for function 16 is not 2, but %d", bytes);
+    ESP_LOGD(TAG, "Number of bytes for function 16 is not %d, but %d", 2 * register_count_requested, data_bytes);
     return nullptr;
   }
-
+/*
   uint8_t value_high_byte = request_data[5];
   uint8_t value_low_byte = request_data[6];
   uint16_t value = (value_high_byte << 8) | value_low_byte;
-
+*/
   uint16_t data_model_address = 40001 + address;
 
+  vector<ModbusData*> *split_data = new vector<ModbusData*>;
+  for (uint8_t i = 0; i < register_count_requested; ++i) {
+    ModbusData *modbus_data_for_register = new ModbusData;
+    modbus_data_for_register->address = start_address + i;
+    uint8_t value_high_byte = request_data[i * 2 + 5];
+    uint8_t value_low_byte = request_data[i * 2 + 6];
+    modbus_data_for_register->value = (value_high_byte << 8) | value_low_byte;
+    split_data->push_back(modbus_data_for_register);
+  }
+/*
   vector<ModbusData*> *split_data = new vector<ModbusData*>;
   ModbusData *modbus_data = new ModbusData;
   modbus_data->address = address;
   modbus_data->value = value;
   split_data->push_back(modbus_data);
-
+*/
   return split_data;
 }
 
