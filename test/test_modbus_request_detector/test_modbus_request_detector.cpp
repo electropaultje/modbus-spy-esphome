564b081a3fda0ea045de95c4851768b85cadd324
Paul de Jong <paul.m.de.jong@gmail.com>
2026-03-10 09:32:22 +0100

    Test for request detector

--- test/test_modbus_request_detector/test_modbus_request_detector.cpp
+++ test/test_modbus_request_detector/test_modbus_request_detector.cpp
@@ -444,6 +444,50 @@ void test_modbus_request_detector_request_function_15() {
   TEST_ASSERT_EQUAL_UINT8(0x01, actual_data[6]);
 }
 
+void test_modbus_request_detector_request_function_16() {
+  // Arrange
+  FakeUartInterface fake_uart_interface;
+  uint8_t fake_data[] = { 0x01, 0x10, 0x07, 0xCF, 0x00, 0x01, 0x02, 0xD5, 0x03 };
+  ModbusRequestDetector modbus_request_detector(&fake_uart_interface);
+  bool uart_task_should_stop = false;
+  FakeUartInterfaceTaskArgs args = { 
+    .uart_interface = &fake_uart_interface,
+    .initial_delay_in_ms = 0,
+    .delay_between_bytes_in_us = 573,
+    .data_to_return = fake_data,
+    .len_of_data_to_return = 9,
+    .should_stop = &uart_task_should_stop
+  };
+  TaskHandle_t uart_task_handle { nullptr };
+  xTaskCreatePinnedToCore(fake_uart_interface_task,
+                    "fake_uart_interface_task", // name
+                    30000,                      // stack size (in words)
+                    &args,                      // input params
+                    1,                          // priority
+                    &uart_task_handle,          // Handle, not needed
+                    0                           // core
+  );
+
+  // Act
+  ModbusFrame *request_frame = modbus_request_detector.detect_request();
+  uart_task_should_stop = true;
+  // Delay 5 ms to make sure that the fake uart task is done
+  delay(5);
+
+  // Assert
+  TEST_ASSERT_FALSE(nullptr == request_frame);
+  TEST_ASSERT_EQUAL_UINT8(0x01, request_frame->get_address());
+  TEST_ASSERT_EQUAL_UINT8(0x10, request_frame->get_function());
+  TEST_ASSERT_EQUAL_UINT8(5, request_frame->get_data_length());
+  
+  const uint8_t *actual_data = request_frame->get_data();
+  TEST_ASSERT_EQUAL_UINT8(0x07, actual_data[0]);
+  TEST_ASSERT_EQUAL_UINT8(0xCF, actual_data[1]);
+  TEST_ASSERT_EQUAL_UINT8(0x00, actual_data[2]);
+  TEST_ASSERT_EQUAL_UINT8(0x01, actual_data[3]);
+  TEST_ASSERT_EQUAL_UINT8(0x02, actual_data[4]);
+}
+
 void test_modbus_request_detector_unsupported_function_results_in_nullptr() {
   // Arrange
   FakeUartInterface fake_uart_interface;
