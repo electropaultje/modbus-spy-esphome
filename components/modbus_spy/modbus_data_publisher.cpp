37fb9572d8be64425fa452568f4a975cc3f38df6
Paul de Jong <paul.m.de.jong@gmail.com>
2026-03-11 20:27:25 +0100

    Implement data publisher

--- components/modbus_spy/modbus_data_publisher.cpp
+++ components/modbus_spy/modbus_data_publisher.cpp
@@ -123,7 +123,8 @@ uint16_t ModbusDataPublisher::convert_pdu_address_to_data_model_address(uint8_t 
   uint16_t modbus_data_model_address { 0 };
   switch (function) {
     case 3: [[fallthrough]];
-    case 6: {
+    case 6: [[fallthrough]];
+    case 16: {
       modbus_data_model_address = pdu_address + 40001;
       break;
     }
