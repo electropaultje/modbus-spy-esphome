5192512f067a42592299b4a554b1e96260607425
Paul de Jong <paul.m.de.jong@gmail.com>
2026-03-15 17:19:12 +0100

    Implementation for binary_sensor

--- components/modbus_spy/binary_sensor.py
+++ components/modbus_spy/binary_sensor.py
@@ -12,16 +12,26 @@ ModbusBinarySensor = modbus_spy_ns.class_('ModbusBinarySensor', binary_sensor.Bi
 CONF_MODBUS_SPY_ID = 'modbus_spy_id'
 CONF_DEVICE_ADDRESS = 'device_address'
 CONF_REGISTER_ADDRESS = 'register_address'
+CONF_RAW_REGISTER_ADDRESS = 'raw_register_address'
 CONF_BIT = 'bit'
 
-CONFIG_SCHEMA = binary_sensor.binary_sensor_schema().extend({
-    cv.GenerateID(CONF_MODBUS_SPY_ID): cv.use_id(ModbusSpy),
-    cv.Required(CONF_DEVICE_ADDRESS): int,
-    cv.Required(CONF_REGISTER_ADDRESS): int,
-    cv.Optional(CONF_BIT, default=-1): cv.int_range(-1, 15)
-})
+CONFIG_SCHEMA = cv.All(
+    binary_sensor.binary_sensor_schema().extend({
+        cv.GenerateID(CONF_MODBUS_SPY_ID): cv.use_id(ModbusSpy),
+        cv.Required(CONF_DEVICE_ADDRESS): int,
+        cv.Optional(CONF_REGISTER_ADDRESS): int,
+        cv.Optional(CONF_RAW_REGISTER_ADDRESS): int,
+        cv.Optional(CONF_BIT, default=-1): cv.int_range(-1, 15)
+    }),
+    cv.has_at_least_one_key(CONF_REGISTER_ADDRESS, CONF_RAW_REGISTER_ADDRESS)
+)
 
 async def to_code(config):
     modbus_spy = await cg.get_variable(config[CONF_MODBUS_SPY_ID])
-    modbus_binary_sensor = modbus_spy.create_binary_sensor(config[CONF_DEVICE_ADDRESS], config[CONF_REGISTER_ADDRESS], config[CONF_BIT])
+    if CONF_REGISTER_ADDRESS in config:
+        register_address = config[CONF_REGISTER_ADDRESS]
+    else:
+        # Assume holding register; convert raw address to data model address
+        register_address = config[CONF_RAW_REGISTER_ADDRESS] + 40001
+    modbus_binary_sensor = modbus_spy.create_binary_sensor(config[CONF_DEVICE_ADDRESS], register_address, config[CONF_BIT])
     await binary_sensor.register_binary_sensor(modbus_binary_sensor, config)
